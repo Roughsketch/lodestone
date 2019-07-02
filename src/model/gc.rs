e20c456203f548943eb251b9acc7d0d72d78cf03
Maid Dog <maiddogsrl@gmail.com>
2019-07-01 22:46:46 -0500

    Add FromStr impls for GrandCompany and Language

--- src/model/gc.rs
+++ src/model/gc.rs
@@ -1,7 +1,28 @@
+use failure::Fail;
+use std::str::FromStr;
+
+#[derive(Clone, Debug, Fail)]
+#[fail(display = "Invalid grand company string '{}'", _0)]
+pub struct GrandCompanyParseError(String);
+
 #[derive(Clone, Copy, Debug, Eq, PartialEq, Ord, PartialOrd, Hash)]
 pub enum GrandCompany {
     Maelstrom,
     TwinAdder,
     ImmortalFlames,
     Unaffiliated,
+}
+
+impl FromStr for GrandCompany {
+    type Err = GrandCompanyParseError;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        match &*s.to_uppercase() {
+            "MAELSTROM" => Ok(GrandCompany::Maelstrom),
+            "ORDER OF THE TWIN ADDER" | "TWIN ADDER" => Ok(GrandCompany::TwinAdder),
+            "IMMORTAL FLAMES" => Ok(GrandCompany::ImmortalFlames),
+            "" | "NONE" | "UNAFFILIATED" => Ok(GrandCompany::Unaffiliated),
+            x => Err(GrandCompanyParseError(x.into())),
+        }
+    }
 }
--- src/model/language.rs
+++ src/model/language.rs
@@ -1,7 +1,28 @@
+use failure::Fail;
+use std::str::FromStr;
+
+#[derive(Clone, Debug, Fail)]
+#[fail(display = "Invalid language string '{}'", _0)]
+pub struct LanguageParseError(String);
+
 #[derive(Clone, Copy, Debug, Eq, PartialEq, Ord, PartialOrd, Hash)]
 pub enum Language {
     Japanese,
     English,
     German,
     French,
+}
+
+impl FromStr for Language {
+    type Err = LanguageParseError;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        match &*s.to_uppercase() {
+            "JAPANESE" | "JA" => Ok(Language::Japanese),
+            "ENGLISH" | "EN" => Ok(Language::English),
+            "GERMAN" | "DE" => Ok(Language::German),
+            "FRENCH" | "FR" => Ok(Language::French),
+            x => Err(LanguageParseError(x.into())),
+        }
+    }
 }
